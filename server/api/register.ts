8a76c13508405907d92d14bb4f9b8f8eb9a77b9e
Aneks1 <proames75yt@gmail.com>
2024-08-03 17:37:34 -0500

    jashd

--- server/api/register.ts
+++ server/api/register.ts
@@ -1,10 +1,11 @@
 import { defineEventHandler, getQuery, readBody } from 'h3'
 import userModel from './mongoSchemas/userSchema'
-
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGO_URL as string);
 export default defineEventHandler(async (event) => {
     const req = await readBody(event)
+    mongoose.connect(process.env.MONGO_URL as string);
     console.log(req)
-    return { status: 200, data: req, mongo: process.env.MONGO_URL, model: userModel }
     if (
         await userModel.findOne({ email: req.email, phone: req.phone })
     ) return { status: 201, data: { user: await userModel.findOne({ email: req.email, phone: req.phone }) } }
@@ -13,7 +14,9 @@ export default defineEventHandler(async (event) => {
         await userModel.findOne({ email: req.email }) || 
         await userModel.findOne({ phone: req.phone }) 
     ) return { status: 409, error: 'Email or phone is already in use' }
-
+    else {
+        return { status: 200, mesage: 'no' }
+    }
     const data = new userModel({ email: req.email, phone: req.phone });
     await data.save();
     return { status: 201, data: { user: data } }
